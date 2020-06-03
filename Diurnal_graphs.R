6cb52067b6c5af3b06587b0d3a5f7df147aafe43
Ben Miller <benjamindmiller42@gmail.com>
2020-06-02 23:41:18 -0400

    Did some commenting and updating of the description.Removed redundant libraries.

--- Diurnal_graphs.R
+++ Diurnal_graphs.R
@@ -1,19 +1,15 @@
 #----
-# Title: Graph outputs of SURF data
+# Title: Diurnal Graph output from 2017 SURF Data
 # Author: Ben Miller
 # Date Started: 20181105
-# Date updated: 20190518
-# Description: Creates graphs of level by level analyses
+# Date updated: 20200602
+# Description: Creates Diurnal pattern graphs of various variables that are then arranged in
+#              a grid. The calculations are done within a function that can be seen below. 
+#              Topt can be applied to the graphs at the end of the user defined variables.
 #----
 
-library(ggplot2)
 library(tidyverse)
-library(plyr)
-library(Hmisc)
-library(Rmisc)
 library(ggpubr)
-library(lubridate)
-library(dplyr)
 
 #Turns off current graphics
 graphics.off()
@@ -22,43 +18,44 @@ graphics.off()
 data_master <- read.csv("SURF_data_master.csv", header = TRUE, sep = ",")
 
 # Sets Z-value used for CI calculation
-Z <- 1.960
+Z <- 1.960 #95 percent
 
 # Sets average Topt Value with confidence intervals for graphic display and time above estimation
-T_opt <- c(29,30,31)
+T_opt <- c(29,30,31) #estimates for now, will change with updated values from Kelsey
 
-#makes a vector of the variables desired for output
+# A ector of the axis labels desired for output
 
 plot_labels <- c(expression(Delta*"T"*" (°C)"), expression("T"["leaf"]*" (°C)"),
                  expression(T["air"]*" (°C)"),
                  bquote("PPFD" ~ umol/s/m^2), "Vapor Pressure Deficit (kPa)")
 
-# list of variables to make plots of. ***VARIBLES MUST MATCH THE ORDER IN THE ABOVE VECTOR****
+# A list of variables to make plots of. 
+# ***VARIBLES MUST MATCH THE ORDER WITH THE ABOVE VECTOR****
+
 plot_variables <- c("delta_temp", "leaf_temp_c", "air_temp_c", "ppfd_mes", "vpd")
 
 ##########------------------- END OF USER DEFINED INPUTS ------------------------------##########
-# Sets data types, creates easily averaged "time" column for data, and calculates delta T
+# Sets data types, creates easily averaged "time" column for data
 data_master$halfhour <- as.POSIXlt(data_master$halfhour)
 data_master$sens_hgt <- as.factor(data_master$sens_hgt)
 
 data_master$hour <- as.numeric(hour(data_master$halfhour))
 data_master$minutes <- as.numeric(minute(data_master$halfhour))
 data_master$time <- data_master$hour + ifelse(data_master$minutes == 30, .5, 0)
-
-data_master$delta_temp <- data_master$leaf_temp_c - data_master$air_temp_c
-
-
-#Uses dplyr to remove some columns
 data_master <- data_master %>%
   select(-c(halfhour,X, hour, minutes))
 
+# Delta T Calculation
+data_master$delta_temp <- data_master$leaf_temp_c - data_master$air_temp_c
 
-# Function to make average calculations and create a plot of the output of a diurnal pattern
+# Function to perform average calculations and create plots 0of the output of the diurnal patterns
 diurnal_plot_function <- function(input_df, var_vector, lab_vector, z_value) {
   #initializes empty plot list
   plots <- c()
+  # loops through the variable vector, and for each variable in the vector does measures
+  # of central tendancy and makes a plot of its diurnal pattern.
   for (i in 1:length(var_vector)) {
-    print(paste0("making ",var_vector[i], " diurnal graph"))
+    print(paste0("Making ",var_vector[i], " diurnal graph"))
     input_df$var <- input_df[, var_vector[i]]
     includedvars <- c("var", "sens_hgt", "time")
     out_df <- input_df %>%
@@ -85,12 +82,14 @@ diurnal_plot_function <- function(input_df, var_vector, lab_vector, z_value) {
       scale_x_continuous(breaks = c(0, 4, 8, 12, 16, 20),
                          labels = c("0:00", "4:00", "8:00", "12:00",
                                     "16:00", "20:00"))
+    # Finds the air temp graphs based on theier identifying string and
+    # adds the Topt information to the plots. Also matches the scales
+    # of the two plots for ease of comparison.
     if (grepl("temp_c", var_vector[i])){
       plots[[var_vector[i]]] <- plots[[var_vector[i]]] +
         scale_y_continuous(breaks = c(24, 26, 28, 30)) +
-        # Adds error of Topt measurements as well as Topt to the air and leaf temp graphs
         geom_hline(yintercept = median(T_opt),
-                   linetype = 2, size = 2) + 
+                   linetype = 1, size = 1) + 
         geom_hline(yintercept = min(T_opt),
                    linetype = 6, size = 1) + 
         geom_hline(yintercept = max(T_opt),
@@ -103,9 +102,15 @@ diurnal_plot_function <- function(input_df, var_vector, lab_vector, z_value) {
 diurnal_plot_function(data_master, plot_variables, plot_labels, Z)
 
 # Creates grid of all graphs
+print("Saving output of all diurnal graphs to file")
+
+# Turns on a graphics device and saves the graphs all in a grid to a .tiff file. Filename must have .tiff at the end
 tiff(filename = "Figure_2_Diunal_Pattern.tiff",
      width = 720, height = 1080, units = "px",
      compression = "none")
+
+# Places all the plots into a grid, mess with the outputs to get different bits where
+# they look nice. Would be difficult to automate that process as computers have no taste.
 ggarrange(Diurnal_Plots$delta_temp,
           nrow = 3, legend = "right",
           hjust = -4.25, vjust = 3,
@@ -113,13 +118,13 @@ ggarrange(Diurnal_Plots$delta_temp,
           ggarrange(Diurnal_Plots$leaf_temp_c,
                     Diurnal_Plots$air_temp_c,
                     ncol = 2, legend = "none",
-                    hjust = -4.25, vjust = 6,
+                    hjust = -4.25, vjust = 12,
                     labels = c("(b)", "(c)")),
           ggarrange(Diurnal_Plots$vpd, Diurnal_Plots$ppfd_mes,
                     ncol = 2, legend = "none",
                     hjust = -5.25, vjust =  4,
                     labels = c("(d)", "(e)")))
-
 dev.off()
 
-# #End of script
+print("Finished")
+# End of script
